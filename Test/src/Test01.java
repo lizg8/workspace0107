7c212ad9c94adcc3cb3ee64d736647aba60478a2
zhigao.li <zhigao.li@luckincoffee.com>
2019-05-17 01:34:18 +0800

    aa

---
+++ Test/src/Test01.java
@@ -0,0 +1,24 @@
+
+public class Test01 {
+
+	
+	public static void main (String []args){
+		main001();
+		
+		int counter=0;
+		int i=0;
+		int j=0;
+		for(i++;counter++<10;j++){
+			System.out.println("counter"+counter); 
+			System.out.println("i"+i); 
+			System.out.println("j"+j); 
+			
+		}
+	//	System.out.println("end "+counter); */
+	}
+
+	public static  void main001 (){
+
+		System.out.println("jinla重货ile");
+	}
+}
